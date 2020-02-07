3dfa7a6accb5c0c1eaeb440ffbd076fea79948c5
CM <christofferjcm@gmail.com>
2020-02-07 13:02:40 +0100

    admin resx fix

--- Winforms/G3Systems/Program.cs
+++ Winforms/G3Systems/Program.cs
@@ -17,7 +17,7 @@ namespace G3Systems
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 
-			Application.Run(new Login());
+			Application.Run(new Admin());
 		}
 	}
 }
