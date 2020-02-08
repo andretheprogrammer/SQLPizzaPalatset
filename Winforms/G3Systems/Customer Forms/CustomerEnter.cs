03e93493cb9ab49597229e8dbbade37b4bcbd41f
CM <christofferjcm@gmail.com>
2020-02-08 19:23:04 +0100

    customerenter terminalID

--- Winforms/G3Systems/Customer Forms/CustomerEnter.cs
+++ Winforms/G3Systems/Customer Forms/CustomerEnter.cs
@@ -12,18 +12,19 @@ namespace G3Systems
 {
 	public partial class CustomerEnter : Form
 	{
-		public CustomerEnter()
+		private readonly int _terminalID;
+
+		public CustomerEnter(int terminalID)
 		{
 			InitializeComponent();
+			_terminalID = terminalID;
 		}
 
 		private void NewOrderBtn_Click(object sender, EventArgs e)
 		{
-			Random rnd = new Random();
-			var terminalID = rnd.Next(1, 4);
-			var form = new PickProduct(terminalID);
+			var form = new PickProduct(_terminalID);
 			form.Show();
-			form.Text += $" {terminalID}";
+			form.Text += $" {_terminalID}";
 			this.Hide();
 		}
 
